83a9fd6c94362f0ea35ee82b5acd0687da0b895e
Anders Jacobsson <anders.jacobsson@gmail.com>
2022-01-30 10:07:09 +0100

    Initial skeleton

---
+++ src/main/java/nu/fgv/register/server/SpexregisterApplication.java
@@ -0,0 +1,13 @@
+package nu.fgv.register.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpexregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpexregisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nu/fgv/register/server/SpexregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package nu.fgv.register.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpexregisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
