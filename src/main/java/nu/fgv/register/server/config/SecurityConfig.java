b3c627879a343c80f694758be296c0f86b6e97f4
Anders Jacobsson <anders.jacobsson@gmail.com>
2023-01-28 11:24:56 +0100

    Disabled security for now

--- src/main/java/nu/fgv/register/server/config/SecurityConfig.java
+++ src/main/java/nu/fgv/register/server/config/SecurityConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration(proxyBeanMethods = false)
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfig {
 }
