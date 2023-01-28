c18bf557e13a38d2b6782166f2dbb15eefc3b245
Anders Jacobsson <anders.jacobsson@gmail.com>
2023-01-28 11:30:33 +0100

    Switched to backticks to escape H2 reserved keywords and still work with MySQL

--- src/main/resources/db/migration/V1_12__create_initial_schema_user.sql
+++ src/main/resources/db/migration/V1_12__create_initial_schema_user.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS "user"
+CREATE TABLE IF NOT EXISTS `user`
 (
     id               BIGINT AUTO_INCREMENT NOT NULL,
     created_by       VARCHAR(50)           NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS "user"
     CONSTRAINT pk_user PRIMARY KEY (id)
 );
 
-ALTER TABLE "user"
+ALTER TABLE `user`
     ADD CONSTRAINT uc_user_uid UNIQUE (uid);
 
-CREATE INDEX IDX_USER_ON_UID ON "user" (uid);
+CREATE INDEX IDX_USER_ON_UID ON `user` (uid);
--- src/main/resources/db/migration/V1_16__create_initial_schema_membership.sql
+++ src/main/resources/db/migration/V1_16__create_initial_schema_membership.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS membership
 (
     id         BIGINT AUTO_INCREMENT NOT NULL,
-    "year"     VARCHAR(4)            NOT NULL,
+    `year`     VARCHAR(4)            NOT NULL,
     type       VARCHAR(255)          NOT NULL,
     spexare_id BIGINT                NULL,
     CONSTRAINT pk_membership PRIMARY KEY (id)
