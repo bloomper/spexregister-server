50ce9efa48d3aad5fb661a35ee177fa8f706ebb0
Anders Jacobsson <anders.jacobsson@gmail.com>
2023-08-14 20:07:43 +0200

    Added configuration for customized access token, i.e. to include roles/authorities

--- src/main/java/nu/fgv/register/server/config/SecurityConfig.java
+++ src/main/java/nu/fgv/register/server/config/SecurityConfig.java
@@ -1,8 +1,24 @@
 package nu.fgv.register.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration(proxyBeanMethods = false)
-//@EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        final JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+
+        final JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+
+        return jwtAuthenticationConverter;
+    }
 }
