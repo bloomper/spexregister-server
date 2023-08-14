7af184c3aa941cb09bf472908e48051c66e04375
Anders Jacobsson <anders.jacobsson@gmail.com>
2023-08-14 19:44:15 +0200

    Reverted UTC change

--- src/main/java/nu/fgv/register/server/config/JpaConfig.java
+++ src/main/java/nu/fgv/register/server/config/JpaConfig.java
@@ -2,20 +2,14 @@ package nu.fgv.register.server.config;
 
 import nu.fgv.register.server.spexare.SpexareRepository;
 import nu.fgv.register.server.spexare.SpexareSearchEnabledJpaRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Optional;
-
 @Configuration
-@EnableJpaAuditing(dateTimeProviderRef = "utcDateTimeProvider")
+@EnableJpaAuditing
 @EnableJpaRepositories(
         basePackages = {
                 "nu.fgv.register.server"
@@ -30,11 +24,6 @@ import java.util.Optional;
         }
 )
 public class JpaConfig {
-
-    @Bean
-    public DateTimeProvider utcDateTimeProvider() {
-        return () -> Optional.of(LocalDateTime.now(ZoneOffset.UTC));
-    }
 
     @Configuration
     @EnableJpaRepositories(
