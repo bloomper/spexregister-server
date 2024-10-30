a17b42b066f7019dbf971ab08599f14799a8894f
Anders Jacobsson <anders.jacobsson@gmail.com>
2024-10-30 16:15:52 +0100

    Add favicon to static pages

--- src/main/java/nu/fgv/register/server/config/SecurityConfig.java
+++ src/main/java/nu/fgv/register/server/config/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(authorize ->
                         authorize
+                                .requestMatchers(HttpMethod.GET, "/favicon.ico").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/docs/**").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/settings/**").permitAll()
                                 .anyRequest().authenticated()
